c3711e87aa3390056388df4e273e170d9c5815e2
Paul Landaeta <paulwilkerlf@gmail.com>
2024-01-06 23:35:54 -0400

    init project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let isVisible = true;
-
+var container = document.getElementById("container");
+container.hidden = true;
 function toggleSlides() {
     const slide1 = document.getElementById("slide1");
     const slide2 = document.getElementById("slide2");
@@ -18,7 +19,19 @@ function toggleSlides() {
 
 toggleSlides();
 function audioClick(id, clip) {
-    var audio = document.getElementById("audio1");
+    var audio = document.getElementById("audio1"); 
     audio.play();
 }
+
+function onHeartButtonClick() {
+    var div = document.getElementById("card2");
+    div.hidden = true;
+    container.hidden = false;
+    audioClick();
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("heartButton").addEventListener("click", onHeartButtonClick);
+});
+
 window.addEventListener('scroll', audioClick);
