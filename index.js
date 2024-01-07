b44a0c9017f373a3cf74e2d5b74f3924477469ad
Paul Landaeta <paulwilkerlf@gmail.com>
2024-01-06 22:47:55 -0400

    init project

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function toggleSlides() {
 }
 
 toggleSlides();
+function audioClick(id, clip) {
+    var audio = document.getElementById("audio1");
+    if (audio.paused) {
+        audio.play();
+    }
+    else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+audioClick();
